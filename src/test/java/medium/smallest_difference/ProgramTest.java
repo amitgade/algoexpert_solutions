bf2ed2dfd8c762a56f1e67574bf185b13c58a22e
amitgade <amitgaade@gmail.com>
2021-05-04 19:41:40 +0530

    action-Add Soln to Smallest Diff problem

---
+++ src/test/java/medium/smallest_difference/ProgramTest.java
@@ -0,0 +1,22 @@
+package medium.smallest_difference;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by amitgade on 04-May-2021
+ */
+public class ProgramTest {
+    @Test
+    public void TestCase1() {
+        int[] expected = {28, 26};
+        Assert.assertTrue(
+                Arrays.equals(
+                        Program.smallestDifference(
+                                new int[] {-1, 5, 10, 20, 28, 3}, new int[] {26, 134, 135, 15, 17}),
+                        expected));
+    }
+
+}
