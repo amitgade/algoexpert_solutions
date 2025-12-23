fd629073f5908ae3f033646e294b4f91f1d9fdfc
amitgade <43432815+amitgade@users.noreply.github.com>
2025-12-23 17:04:02 +0530

    action-Remove Duplicates from LinkedList

---
+++ src/main/java/dev/amit/algoexpert/codingquest/easy/removeduplicatesfromlinkedlist/Program.java
@@ -0,0 +1,31 @@
+package dev.amit.algoexpert.codingquest.easy.removeduplicatesfromlinkedlist;
+
+public class Program {
+    // This is an input class. Do not edit.
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    // O(n) time | O(1) space : despite of 2 while loops
+    // this is because we have given sorted linkedList
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        // Write your code here.
+        LinkedList currentNode = linkedList;
+        while (currentNode != null) {
+            LinkedList nextDistinctNode = currentNode.next;
+            while (nextDistinctNode != null && nextDistinctNode.value == currentNode.value) {
+                nextDistinctNode = nextDistinctNode.next;
+            }
+            // we found next distinct node or reached to the end of list
+            currentNode.next = nextDistinctNode; // update next pointer to nestDistinct
+            currentNode = nextDistinctNode; // Now iterate from next distDistinct
+        }
+        return linkedList;
+    }
+}
